e6c51b7c7c05a5abaae0f516bfd6847898e84d99
Giuseppe Nucifora <me@giuseppenucifora.com>
2018-04-19 12:16:06 +0200

    - upgrade pod library - fix iPad 12" recognition

--- Pod/Classes/UIDevice-Hardware.m
+++ Pod/Classes/UIDevice-Hardware.m
@@ -114,10 +114,10 @@
     if ([modelIdentifier isEqualToString:@"iPad6,4"])      return UIDeviceGenerationModeliPadPro97; // http://pdadb.net/index.php?m=specs&id=9981&c=apple_ipad_pro_9.7-inch_a1675_td-lte_32gb_apple_ipad_6,4
     if ([modelIdentifier isEqualToString:@"iPad6,7"])      return UIDeviceGenerationModeliPadPro12; // http://pdadb.net/index.php?m=specs&id=8960&c=apple_ipad_pro_wifi_a1584_128gb
     if ([modelIdentifier isEqualToString:@"iPad6,8"])      return UIDeviceGenerationModeliPadPro12; // http://pdadb.net/index.php?m=specs&id=8965&c=apple_ipad_pro_td-lte_a1652_32gb_apple_ipad_6,8
-    if ([modelIdentifier isEqualToString:@"iPad 7,1"])     return UIDeviceGenerationModeliPadPro12;
-    if ([modelIdentifier isEqualToString:@"iPad 7,2"])     return UIDeviceGenerationModeliPadPro12;
-    if ([modelIdentifier isEqualToString:@"iPad 7,3"])     return UIDeviceGenerationModeliPadPro105;
-    if ([modelIdentifier isEqualToString:@"iPad 7,4"])     return UIDeviceGenerationModeliPadPro105;
+    if ([modelIdentifier isEqualToString:@"iPad7,1"])     return UIDeviceGenerationModeliPadPro12;
+    if ([modelIdentifier isEqualToString:@"iPad7,2"])     return UIDeviceGenerationModeliPadPro12;
+    if ([modelIdentifier isEqualToString:@"iPad7,3"])     return UIDeviceGenerationModeliPadPro105;
+    if ([modelIdentifier isEqualToString:@"iPad7,4"])     return UIDeviceGenerationModeliPadPro105;
     
     // iPod http://theiphonewiki.com/wiki/IPod
     
@@ -224,10 +224,10 @@
     if ([modelIdentifier isEqualToString:@"iPad6,4"])      return @"iPad Pro (9.7 inch) 1G (Cellular)"; // http://pdadb.net/index.php?m=specs&id=9981&c=apple_ipad_pro_9.7-inch_a1675_td-lte_32gb_apple_ipad_6,4
     if ([modelIdentifier isEqualToString:@"iPad6,7"])      return @"iPad Pro (12.9 inch) 1G (Wi-Fi)"; // http://pdadb.net/index.php?m=specs&id=8960&c=apple_ipad_pro_wifi_a1584_128gb
     if ([modelIdentifier isEqualToString:@"iPad6,8"])      return @"iPad Pro (12.9 inch) 1G (Cellular)"; // http://pdadb.net/index.php?m=specs&id=8965&c=apple_ipad_pro_td-lte_a1652_32gb_apple_ipad_6,8
-    if ([modelIdentifier isEqualToString:@"iPad 7,1"])     return @"iPad Pro (12.9 inch) 2G (Wi-Fi)";
-    if ([modelIdentifier isEqualToString:@"iPad 7,2"])     return @"iPad Pro (12.9 inch) 2G (Cellular)";
-    if ([modelIdentifier isEqualToString:@"iPad 7,3"])     return @"iPad Pro (10.5 inch) 1G (Wi-Fi)";
-    if ([modelIdentifier isEqualToString:@"iPad 7,4"])     return @"iPad Pro (10.5 inch) 1G (Cellular)";
+    if ([modelIdentifier isEqualToString:@"iPad7,1"])     return @"iPad Pro (12.9 inch) 2G (Wi-Fi)";
+    if ([modelIdentifier isEqualToString:@"iPad7,2"])     return @"iPad Pro (12.9 inch) 2G (Cellular)";
+    if ([modelIdentifier isEqualToString:@"iPad7,3"])     return @"iPad Pro (10.5 inch) 1G (Wi-Fi)";
+    if ([modelIdentifier isEqualToString:@"iPad7,4"])     return @"iPad Pro (10.5 inch) 1G (Cellular)";
     
     // iPod http://theiphonewiki.com/wiki/IPod
     
